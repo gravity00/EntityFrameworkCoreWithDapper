895c970aef8d02656b0683a26efafad1411a0213
João Pedro Romão Simões <joao.simoes@glintt.com>
2020-12-12 04:05:12 +0000

    poducts controller using just EF context

---
+++ EntityFrameworkCoreWithDapper/Controllers/ProductsController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EntityFrameworkCoreWithDapper.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCoreWithDapper.Controllers
+{
+    [Route("products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public ProductsController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProductModel>> GetAllAsync(CancellationToken ct)
+        {
+            return await _context.Set<ProductEntity>().OrderBy(e => e.Code).Select(p => new ProductModel
+            {
+                Id = p.ExternalId,
+                Code = p.Code,
+                Name = p.Name,
+                Price = p.Price
+            }).ToListAsync(ct);
+        }
+
+        [HttpPost]
+        public async Task<CreateProductResultModel> CreateAsync([FromBody] CreateProductModel model, CancellationToken ct)
+        {
+            var externalId = Guid.NewGuid();
+
+            await _context.Set<ProductEntity>().AddAsync(new ProductEntity
+            {
+                ExternalId = externalId,
+                Code = model.Code,
+                Name = model.Name,
+                Price = model.Price
+            }, ct);
+
+            await _context.SaveChangesAsync(ct);
+
+            return new CreateProductResultModel
+            {
+                Id = externalId
+            };
+        }
+
+        [HttpPost("price/multiply")]
+        public async Task MultiplyPricesAsync([FromBody] MultiplyProductsPriceModel model, CancellationToken ct)
+        {
+            var products = await _context.Set<ProductEntity>().ToListAsync(ct);
+
+            foreach (var product in products)
+            {
+                product.Price *= model.Factor;
+            }
+
+            await _context.SaveChangesAsync(ct);
+        }
+    }
+
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CreateProductModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class CreateProductResultModel
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class MultiplyProductsPriceModel
+    {
+        public decimal Factor { get; set; }
+    }
+}
